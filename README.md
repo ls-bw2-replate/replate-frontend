bd42ab2c981d0d414a91eae03dcb14e70287284d
Ian Be <ian84be@gmail.com>
2019-03-11 14:25:59 -0500

    day1 scaffold basic layout & styles, create SignupVolunteer form

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # replate-frontend
+RePlate
+---
+Feed the Hungry.
+Reduce Waste.
+---
+Ian Belknap - Front End Architect
--- replate/src/App.js
+++ replate/src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import SignupVolunteer from './components/SignupVolunteer';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <nav>
+            <div className="nav__left">
+            <a href="#">Login</a>
+            </div>
+            <div className="nav__right">
+            <a href="#">Home</a>
+            <a href="#">Sign Up</a>
+            </div>
+          </nav>
         </header>
+
+        <main>
+          <SignupVolunteer />
+        </main>
       </div>
     );
   }
